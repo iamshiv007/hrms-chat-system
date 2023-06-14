b55fe23902660e4fbb857feeccd5917fc6102707
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-14 08:34:32 +0530

    Login and SignUp completed

---
+++ src/components/Login.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const LoginUser = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:7070/api/user/login", { name, email })
+      .then((res) => {
+        alert(res.data.message);
+        localStorage.setItem("_id", res.data.user._id);
+        localStorage.setItem("email", res.data.user.email);
+        navigate("/chatRoom");
+      })
+      .catch((err) => alert(err.response.data.message));
+  };
+
+  return (
+    <div
+      style={{
+        background: "pink",
+        width: "40%",
+        margin: "auto",
+        padding: "20px",
+        borderRadius: "5px",
+        marginTop: "10%",
+      }}
+    >
+      <form action="" onSubmit={LoginUser}>
+        <div>
+          <label htmlFor="">Name</label>
+          <input
+            name="name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            style={{ margin: "10px", padding: "10px" }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">email</label>
+          <input
+            name="email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ margin: "10px", padding: "10px" }}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Login"
+          style={{
+            marginTop: "10px",
+            background: "gray",
+            padding: "8px",
+            borderRadius: "4px",
+            color: "white",
+          }}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
